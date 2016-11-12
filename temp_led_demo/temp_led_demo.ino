73dbb2543747cc593451385f84b13284aedc14a7
Aaron Nielsen <apn@apnielsen.com>
2016-11-12 13:42:27 -0500

    added arduino project

---
+++ temp_led_demo/temp_led_demo.ino
@@ -0,0 +1,40 @@
+
+// these are the pins attached to the lilypad 
+const int tempPin  = A1; // analong pin attached to temperature sensor
+const int redPin   =  9; 
+const int bluePin  = 10; 
+const int greenPin = 11; 
+
+// A/D settings for reading temperature sensor
+const int MAX_TEMP_BITS = 10;             // number of bits used to read temperature analog
+const int MAX_TEMP_INT = 2^MAX_TEMP_BITS; // integer steps used to read temperature
+const float MAX_TEMP_RANGE_VOLTS = 5.0;   // maximum voltage range of temperature
+
+// this function adapted from this gist: https://gist.github.com/jamesotron/766994
+// colors range from [0,255] == [off,full on]
+void setColorRgb(unsigned int red, unsigned int green, unsigned int blue){
+  analogWrite(redPin,red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue); 
+}
+
+float tempRead(){  
+  // reads from the temperature sensor and returns the temperature is degree Centigrade
+  //
+  // sensor output at 0 deg C is 0.5 V. 
+  // linear coefficient is 10 mV/dec C
+  // Microchip  MCP 9700, SparkFun part number DEV-08777 
+  return ((float) analogRead(tempPin)) * MAX_TEMP_RANGE_VOLTS / (float) MAX_TEMP_INT  * 0.01 + 0.5;   
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // initialize RGB LEDs to off
+  setColorRgb(0,0,0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
